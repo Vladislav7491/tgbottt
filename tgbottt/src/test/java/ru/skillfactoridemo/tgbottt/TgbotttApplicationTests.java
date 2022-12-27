5fe670fd3038c86e470dbba0e1a9f29f362dbaa6
Vladislav7491 <104887176+Vladislav7491@users.noreply.github.com>
2022-12-27 21:59:43 +0500

    Add files via upload

---
+++ tgbottt/src/test/java/ru/skillfactoridemo/tgbottt/TgbotttApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillfactoridemo.tgbottt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgbotttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
